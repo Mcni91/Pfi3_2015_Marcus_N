62aa175adcefd6d4d62a34d17c7cf8fcea330e6d
Mcni91 <mcni.91@gmail.com>
2015-04-13 21:59:25 +0200

    "No changes" my ass...

---
+++ Nilsson_M_Assignment2/src/main/java/se/mah/nilsson_m_assignment2/MainScreenFragment.java
@@ -0,0 +1,28 @@
+package se.mah.nilsson_m_assignment2;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainScreenFragment extends Fragment {
+
+
+    public MainScreenFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_screen, container, false);
+    }
+}
